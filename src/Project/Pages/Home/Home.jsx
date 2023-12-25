8081b8966e38e788a0d2fda89daeb5796e335ea1
lougerb <glouien@gmail.com>
2023-12-25 10:29:07 +0800

    added prototype of animation

--- src/Project/Pages/Home/Home.jsx
+++ src/Project/Pages/Home/Home.jsx
@@ -9,8 +9,73 @@ function Home() {
                     </h1>
                 </div>
                 <div className="home-intro__animated-bg">
-                    <div className="home-intro__animated-bg__frame home-intro__animated-bg__frame--1"></div>
-                    <div className="home-intro__animated-bg__frame home-intro__animated-bg__frame--2"></div>
+                    <div className="home-intro__animated-bg__frame home-intro__animated-bg__frame--1">
+                        <div className="home-intro__animated-bg__frame__items">
+                            <div className="home-intro__animated-bg__frame__items__item home-intro__animated-bg__frame__items__item--1">
+                                <div className="home-intro__animated-bg__frame__items__item__icon"></div>
+                            </div>
+                            <div className="home-intro__animated-bg__frame__items__item home-intro__animated-bg__frame__items__item--2">
+                                <div className="home-intro__animated-bg__frame__items__item__icon"></div>
+                            </div>
+                            <div className="home-intro__animated-bg__frame__items__item home-intro__animated-bg__frame__items__item--3">
+                                <div className="home-intro__animated-bg__frame__items__item__icon"></div>
+                            </div>
+                            <div className="home-intro__animated-bg__frame__items__item home-intro__animated-bg__frame__items__item--4">
+                                <div className="home-intro__animated-bg__frame__items__item__icon"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="home-intro__animated-bg__frame home-intro__animated-bg__frame--2">
+                        <div className="home-intro__animated-bg__frame__items">
+                            <div className="home-intro__animated-bg__frame__items__item home-intro__animated-bg__frame__items__item--1">
+                                <div className="home-intro__animated-bg__frame__items__item__icon"></div>
+                            </div>
+                            <div className="home-intro__animated-bg__frame__items__item home-intro__animated-bg__frame__items__item--2">
+                                <div className="home-intro__animated-bg__frame__items__item__icon"></div>
+                            </div>
+                            <div className="home-intro__animated-bg__frame__items__item home-intro__animated-bg__frame__items__item--3">
+                                <div className="home-intro__animated-bg__frame__items__item__icon"></div>
+                            </div>
+                            <div className="home-intro__animated-bg__frame__items__item home-intro__animated-bg__frame__items__item--4">
+                                <div className="home-intro__animated-bg__frame__items__item__icon"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div className="home-intro__animated-bg bigger-animated-bg">
+                    <div className="home-intro__animated-bg__frame home-intro__animated-bg__frame--1">
+                        <div className="home-intro__animated-bg__frame__items">
+                            <div className="home-intro__animated-bg__frame__items__item home-intro__animated-bg__frame__items__item--1">
+                                <div className="home-intro__animated-bg__frame__items__item__icon"></div>
+                            </div>
+                            <div className="home-intro__animated-bg__frame__items__item home-intro__animated-bg__frame__items__item--2">
+                                <div className="home-intro__animated-bg__frame__items__item__icon"></div>
+                            </div>
+                            <div className="home-intro__animated-bg__frame__items__item home-intro__animated-bg__frame__items__item--3">
+                                <div className="home-intro__animated-bg__frame__items__item__icon"></div>
+                            </div>
+                            <div className="home-intro__animated-bg__frame__items__item home-intro__animated-bg__frame__items__item--4">
+                                <div className="home-intro__animated-bg__frame__items__item__icon"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="home-intro__animated-bg__frame home-intro__animated-bg__frame--2">
+                        <div className="home-intro__animated-bg__frame__items">
+                            <div className="home-intro__animated-bg__frame__items__item home-intro__animated-bg__frame__items__item--1">
+                                <div className="home-intro__animated-bg__frame__items__item__icon"></div>
+                            </div>
+                            <div className="home-intro__animated-bg__frame__items__item home-intro__animated-bg__frame__items__item--2">
+                                <div className="home-intro__animated-bg__frame__items__item__icon"></div>
+                            </div>
+                            <div className="home-intro__animated-bg__frame__items__item home-intro__animated-bg__frame__items__item--3">
+                                <div className="home-intro__animated-bg__frame__items__item__icon"></div>
+                            </div>
+                            <div className="home-intro__animated-bg__frame__items__item home-intro__animated-bg__frame__items__item--4">
+                                <div className="home-intro__animated-bg__frame__items__item__icon"></div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </section>
         </div>
